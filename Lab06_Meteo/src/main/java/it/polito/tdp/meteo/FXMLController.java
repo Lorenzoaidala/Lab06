a13616077e1b35e60984f9b32a17c2aa2a10d04f
Lorenzo Aidala <Lorenzo Aidala@DESKTOP-21723CJ>
2021-05-21 10:44:57 +0200

    Il metodo trova sequenza ritorna "null"

--- Lab06_Meteo/src/main/java/it/polito/tdp/meteo/FXMLController.java
+++ Lab06_Meteo/src/main/java/it/polito/tdp/meteo/FXMLController.java
@@ -5,8 +5,10 @@
 package it.polito.tdp.meteo;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import it.polito.tdp.meteo.model.Citta;
 import it.polito.tdp.meteo.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -15,55 +17,71 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 
 public class FXMLController {
-	
+
 	private Model model;
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
-    private ResourceBundle resources;
+	@FXML // ResourceBundle that was given to the FXMLLoader
+	private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
-    private URL location;
+	@FXML // URL location of the FXML file that was given to the FXMLLoader
+	private URL location;
 
-    @FXML // fx:id="boxMese"
-    private ChoiceBox<?> boxMese; // Value injected by FXMLLoader
+	@FXML // fx:id="boxMese"
+	private ChoiceBox<Integer> boxMese; // Value injected by FXMLLoader
 
-    @FXML // fx:id="btnUmidita"
-    private Button btnUmidita; // Value injected by FXMLLoader
+	@FXML // fx:id="btnUmidita"
+	private Button btnUmidita; // Value injected by FXMLLoader
 
-    @FXML // fx:id="btnCalcola"
-    private Button btnCalcola; // Value injected by FXMLLoader
+	@FXML // fx:id="btnCalcola"
+	private Button btnCalcola; // Value injected by FXMLLoader
 
-    @FXML // fx:id="txtResult"
-    private TextArea txtResult; // Value injected by FXMLLoader
+	@FXML // fx:id="txtResult"
+	private TextArea txtResult; // Value injected by FXMLLoader
 
-    @FXML
-    void doCalcolaSequenza(ActionEvent event) {
-    	
-    }
+	@FXML
+	void doCalcolaSequenza(ActionEvent event) {
+		txtResult.clear();
+		Integer m = boxMese.getValue();
+		if(m!=null) {
+			List<Citta> best = model.trovaSequenza(m);
 
-    @FXML
-    void doCalcolaUmidita(ActionEvent event) {
+			txtResult.appendText(String.format("Sequenza ottima per il mese %s\n", Integer.toString(m)));
+			txtResult.appendText(best+ "\n");
+		}
+	}
 
-    }
+	@FXML
+	void doCalcolaUmidita(ActionEvent event) {
+		txtResult.clear();
+		Integer m = boxMese.getValue();
+		if(m!=null) {
+			
+			txtResult.appendText(String.format("Dati umidità per il mese %s\n", Integer.toString(m)));
+			for(Citta c : model.getLeCitta()) {
+				Double u = model.getUmiditaMedia(m, c.getNome());
+				txtResult.appendText(String.format("Città %s: umidità %f ", c.getNome(), u));
+			}
+		}
+	}
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
-        assert boxMese != null : "fx:id=\"boxMese\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert btnUmidita != null : "fx:id=\"btnUmidita\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert btnCalcola != null : "fx:id=\"btnCalcola\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+	@FXML // This method is called by the FXMLLoader when initialization is complete
+	void initialize() {
+		assert boxMese != null : "fx:id=\"boxMese\" was not injected: check your FXML file 'Scene.fxml'.";
+		assert btnUmidita != null : "fx:id=\"btnUmidita\" was not injected: check your FXML file 'Scene.fxml'.";
+		assert btnCalcola != null : "fx:id=\"btnCalcola\" was not injected: check your FXML file 'Scene.fxml'.";
+		assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
 
-    }
-    
-    public void setChoiceBox() {
-    	for(int mese=1; mese<=12;mese++) {
-        	boxMese.getItems().add(mese);
-    	}
-    }
-    
-    public void setModel(Model model) {
-    	this.model = model;
-    	
-    }
+	}
+
+	public void setChoiceBox() {
+		for(int mese=1; mese<=12;mese++) {
+			boxMese.getItems().add(mese);
+		}
+	}
+
+	public void setModel(Model model) {
+		this.model = model;
+		setChoiceBox();
+	}
 }
 
--- Lab06_Meteo/src/main/java/it/polito/tdp/meteo/model/Model.java
+++ Lab06_Meteo/src/main/java/it/polito/tdp/meteo/model/Model.java
@@ -6,12 +6,12 @@ import java.util.List;
 import it.polito.tdp.meteo.DAO.MeteoDAO;
 
 public class Model {
-	
+
 	private final static int COST = 100;
 	private final static int NUMERO_GIORNI_CITTA_CONSECUTIVI_MIN = 3;
 	private final static int NUMERO_GIORNI_CITTA_MAX = 6;
 	private final static int NUMERO_GIORNI_TOTALI = 15;
-	
+
 	private List<Citta> leCitta;
 	private List<Citta> best;
 
@@ -19,20 +19,23 @@ public class Model {
 		MeteoDAO meteoDao = new MeteoDAO();
 		leCitta = meteoDao.getAllCitta();
 	}
-	
+
+	public List<Citta> getLeCitta(){
+		return this.leCitta;
+	}
 
 	// of course you can change the String output with what you think works best
 	public Double getUmiditaMedia(int mese, String localita) {
 		MeteoDAO meteoDao = new MeteoDAO();
 		return meteoDao.getAvgRilevamentiLocalitaMese(mese, localita);
 	}
-	
+
 	// of course you can change the String output with what you think works best
 	public List<Citta> trovaSequenza(int mese) {
 		List<Citta> parziale = new ArrayList<Citta>();
 		this.best = null;
 		MeteoDAO dao = new MeteoDAO();
-		
+
 		for(Citta c : leCitta) {
 			c.setRilevamenti(dao.getAllRilevamentiLocalitaMese(mese, c.getNome()));
 		}
@@ -43,8 +46,8 @@ public class Model {
 
 	private void cerca(List<Citta> parziale, int livello) {
 		if(livello == NUMERO_GIORNI_TOTALI) {
-			Double costo = (parziale);
-			
+			Double costo = calcolaCosto(parziale);
+
 			if(best==null || costo<calcolaCosto(best)) {
 				best = new ArrayList<Citta>(parziale);
 			} else {
@@ -57,18 +60,41 @@ public class Model {
 				}
 			}
 		}
-		
-		
+
+
 	}
 
 
-	private Double (List<Citta> parziale) {
-		double costo =0.0;
+	private boolean aggiuntaValida(List<Citta> parziale, Citta prova) {
+		int cont = 0;
+		for(Citta precedente : parziale) {
+			if(precedente.equals(prova)) {
+				cont++;
+			}
+		}
+		if(cont>=NUMERO_GIORNI_CITTA_MAX)
+			return false;
+		if(parziale.size()==0)
+		return true;
+		if(parziale.size()==1 || parziale.size()==2) {
+			return parziale.get(parziale.size()-1).equals(prova);
+		}
+		if(parziale.get(parziale.size()-1).equals(prova))
+			return true;
+		if(parziale.get(parziale.size()-1).equals(parziale.get(parziale.size()-2)) && parziale.get(parziale.size()-2).equals(parziale.get(parziale.size()-3)))
+			return true;
 		
+		return false;
+	}
+
+
+	private Double calcolaCosto(List<Citta> parziale) {
+		double costo =0.0;
+
 		for(int giorno =1;giorno<=NUMERO_GIORNI_TOTALI;giorno++) {
 			Citta c = parziale.get(giorno-1);
 			double umidita = c.getRilevamenti().get(giorno-1).getUmidita();
-			
+
 			costo+=umidita;
 		}
 		for(int giorno =2; giorno<=NUMERO_GIORNI_TOTALI; giorno++) {
@@ -78,6 +104,6 @@ public class Model {
 		}
 		return costo;
 	}
-	
+
 
 }
